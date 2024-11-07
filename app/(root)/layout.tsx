cae9b8873a0351aaca203dbe3b83e9b7becb9482
Abhijeet <abhijeetsachan.088@gmail.com>
2024-11-07 21:28:15 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,28 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "Virtuale",
+  description: "Vedio Calling App",
+  icons:{
+    icon: '/icons/logo.svg'
+  }
+};
+
+
+
+
+const RootLayout = ({children}:{children: ReactNode}) => {
+  return (
+    <main>
+        <StreamVideoProvider>
+
+        {children}
+        </StreamVideoProvider>
+        
+    </main>
+  )
+}
+
+export default RootLayout
